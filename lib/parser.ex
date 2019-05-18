f2e2b9d32e0154b8853e54a6847943997b76f8e4
Doug ClymerOlson <doug@yarina.org>
2019-05-18 03:45:51 +0000

    applied formatter

--- lib/parser.ex
+++ lib/parser.ex
@@ -26,7 +26,8 @@ defmodule XMLStreamTools.Parser do
   @doc """
   Basic XML Parser, parses to a stream of tags and text.  This makes it possible to process XML as a stream.
   """
-  defparsec :element, parsec(:node), debug: true # |> concat(repeat(parsec(:node))) |> eos()
+  # |> concat(repeat(parsec(:node))) |> eos()
+  defparsec(:element, parsec(:node), debug: true)
 
   tag_chars = [?a..?z, ?A..?Z, ?0..?9, ?_]
   tag_id = ascii_string(tag_chars, min: 1)
@@ -34,8 +35,9 @@ defmodule XMLStreamTools.Parser do
   namespace = tag_id |> ignore(string(":")) |> tag(:namespace) |> reduce(:deconvolute)
   tag = optional(namespace) |> concat(tag_id) |> tag(:tag) |> reduce(:deconvolute)
   ws = ascii_string([?\s], min: 1)
-  
-  defcombinatorp :attribute,
+
+  defcombinatorp(
+    :attribute,
     ignore(optional(ws))
     |> concat(ns_id)
     |> ignore(optional(ws))
@@ -43,13 +45,17 @@ defmodule XMLStreamTools.Parser do
     |> ignore(optional(ws))
     |> parsec(:quoted_string)
     |> reduce(:into_keyword)
+  )
 
-  defcombinatorp :open_tag,
+  defcombinatorp(
+    :open_tag,
     ignore(optional(ws))
     |> ignore(string("<"))
-    |> line() |> byte_offset() |> reduce(:add_loc)
+    |> line()
+    |> byte_offset()
+    |> reduce(:add_loc)
     |> concat(tag)
-    |> optional(parsec(:attribute) |> repeat() |> tag(:attr)) 
+    |> optional(parsec(:attribute) |> repeat() |> tag(:attr))
     |> reduce(:filter_empty_attr)
     |> choice([
       string("/>") |> tag(:close) |> reduce(:set_true),
@@ -58,28 +64,41 @@ defmodule XMLStreamTools.Parser do
     |> reduce(:deconvolute)
     |> reduce(:sort)
     |> unwrap_and_tag(:open_tag)
- 
-  defcombinatorp :close_tag,
+  )
+
+  defcombinatorp(
+    :close_tag,
     ignore(optional(ws))
     |> ignore(string("</"))
-    |> line() |> byte_offset() |> reduce(:add_loc)
+    |> line()
+    |> byte_offset()
+    |> reduce(:add_loc)
     |> concat(tag)
     |> ignore(string(">"))
     |> reduce(:close_deconvolute)
     |> unwrap_and_tag(:close_tag)
+  )
 
-  defcombinatorp :text,
+  defcombinatorp(
+    :text,
     ascii_string([not: ?<], min: 1)
-    |> line() |> byte_offset() |> reduce(:add_loc)
+    |> line()
+    |> byte_offset()
+    |> reduce(:add_loc)
     |> unwrap_and_tag(:text)
+  )
 
-  defcombinatorp :quoted_string,
-    choice([quote_by_delimiter(?"), quote_by_delimiter(?')])   #" help the formatters
+  defcombinatorp(
+    :quoted_string,
+    # " help the formatters
+    choice([quote_by_delimiter(?"), quote_by_delimiter(?')])
+  )
 
-  defcombinatorp :node,
+  defcombinatorp(
+    :node,
     choice([parsec(:open_tag), parsec(:close_tag), parsec(:text)])
+  )
 
-  
   defp reverse(_rest, list, context, _line, _offset) do
     {Enum.reverse(list), context}
   end
@@ -88,34 +107,35 @@ defmodule XMLStreamTools.Parser do
 
   defp close_deconvolute([loc | [{:tag, _} = tag]]), do: [tag] ++ loc
   defp close_deconvolute([loc | [tag]]), do: tag ++ loc
-  
-  defp deconvolute([{tag, [ns|[t]]}]), do: [{tag, t}, ns]
-  defp deconvolute([{tag, [h|_t]}]), do: {tag, h}
-  defp deconvolute([list, {:close, true}=h]), do: [h | list]
+
+  defp deconvolute([{tag, [ns | [t]]}]), do: [{tag, t}, ns]
+  defp deconvolute([{tag, [h | _t]}]), do: {tag, h}
+  defp deconvolute([list, {:close, true} = h]), do: [h | list]
   defp deconvolute([{:tag, name}]), do: [tag: name]
   defp deconvolute([[loc: loc], tag]), do: [{:loc, loc} | tag]
   defp deconvolute([list]), do: list
 
   defp filter_empty_attr([loc, {:tag, tag}, {:attr, []}]), do: [tag: tag] ++ loc
   defp filter_empty_attr([loc, tag, {:attr, []}]), do: tag ++ loc
-  defp filter_empty_attr([loc, {:tag, tag}, attr]), do: [attr | [tag: tag] ] ++ loc
-  defp filter_empty_attr([loc, tag, attr]), do: [attr | tag ] ++ loc
+  defp filter_empty_attr([loc, {:tag, tag}, attr]), do: [attr | [tag: tag]] ++ loc
+  defp filter_empty_attr([loc, tag, attr]), do: [attr | tag] ++ loc
 
   defp set_true([{:close, _}]), do: {:close, true}
 
-  defp add_loc([{[{[], line}] ,offset}]), do: [loc: {line, offset}]
+  defp add_loc([{[{[], line}], offset}]), do: [loc: {line, offset}]
   defp add_loc([{[{[data], line}], offset}]), do: [data, loc: {line, offset}]
 
   defp add_line_and_offset([{[{[{label, tag}], line}], offset}]), do: {label, tag, line, offset}
-#  defp add_line([{[{label, tag}], line}]), do: {label, tag, line}
+  #  defp add_line([{[{label, tag}], line}]), do: {label, tag, line}
 
   defp sort([list]) do
     key_order = [:tag, :namespace, :attr]
     index = fn list, item -> Enum.find_index(list, &Kernel.==(&1, item)) end
     Enum.sort_by(list, fn {k, _} -> index.(key_order, k) end)
   end
-  
+
   def parse_next({"", loc, offset}), do: {:halt, {"", loc, offset}}
+
   def parse_next({xml, loc, offset}) do
     {:ok, [next_item], rest, _, loc, offset} = element__0(xml, [], [], [], loc, offset)
     {[next_item], {rest, loc, offset}}
@@ -129,5 +149,3 @@ defmodule XMLStreamTools.Parser do
     )
   end
 end
-
-
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -7,54 +7,53 @@ defmodule XMLStreamTools.ParserTest do
     |> XMLStreamTools.Parser.parse()
     |> Enum.map(fn x -> x end)
   end
-  
+
   test "test 1" do
     result = parse_xml("<ns:foo a=\"1\" a:b='2'>bar</ns:foo>")
     # IO.inspect(result)
     assert result == [
-      open_tag: [
-        tag: "foo",
-        namespace: "ns",
-        attr: [{"a", "1"}, {"a:b", "2"}],
-        loc: {{1, 0}, 1}
-      ],
-      text: ["bar", {:loc, {{1, 0}, 25}}],
-      close_tag: [tag: "foo", namespace: "ns", loc: {{1, 0}, 27}]
-    ]
+             open_tag: [
+               tag: "foo",
+               namespace: "ns",
+               attr: [{"a", "1"}, {"a:b", "2"}],
+               loc: {{1, 0}, 1}
+             ],
+             text: ["bar", {:loc, {{1, 0}, 25}}],
+             close_tag: [tag: "foo", namespace: "ns", loc: {{1, 0}, 27}]
+           ]
   end
 
   test "test 2" do
     result = parse_xml("<ns:foo a='1'><bar>message</bar></ns:foo>")
     # IO.inspect(result)
     assert result == [
-      {:open_tag, [tag: "foo", namespace: "ns", attr: [{"a", "1"}], loc: {{1, 0}, 1}]},
-      {:open_tag, [tag: "bar", loc: {{1, 0}, 15}]},
-      {:text, ["message", {:loc, {{1, 0}, 26}}]},
-      {:close_tag, [tag: "bar", loc: {{1, 0}, 28}]},
-      {:close_tag, [tag: "foo", namespace: "ns", loc: {{1, 0}, 34}]}
-    ]    
+             {:open_tag, [tag: "foo", namespace: "ns", attr: [{"a", "1"}], loc: {{1, 0}, 1}]},
+             {:open_tag, [tag: "bar", loc: {{1, 0}, 15}]},
+             {:text, ["message", {:loc, {{1, 0}, 26}}]},
+             {:close_tag, [tag: "bar", loc: {{1, 0}, 28}]},
+             {:close_tag, [tag: "foo", namespace: "ns", loc: {{1, 0}, 34}]}
+           ]
   end
-    
-# inputs = [
-#       "<ns:foo a=\"1\" a:b='2'>bar</ns:foo>",
-#       "<ns:biz a='bar'/>",
-#       "<foo><bar>baz</bar>\n</foo>",
-#       "<foo><bar>one</bar><bar>two</bar></foo>",
-# #      "<>bar</>",
-#       "<foo>bar</baz>",
-#       "<foo>bar</foo>oops",
-#       "<foo>bar",
-#       "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">",
-#       "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:tns=\"http://www.witsml.org/wsdl/120\" xmlns:types=\"http://www.witsml.org/wsdl/120/encodedTypes\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soap:Body soap:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><q1:WMLS_GetVersion xmlns:q1=\"http://www.witsml.org/message/120\"/></soap:Body></soap:Envelope>",
-#       "hi, this is a <bold>bold</bold> statement"
-# ]
 
-# for input <- inputs do
-#     IO.puts(input)
-#     stream = XMLStream.Parser.parse(input)
-#     Enum.each(stream, fn x -> IO.inspect(x) end)
-#     |> IO.inspect()
-#     IO.puts("")
-# end
+  # inputs = [
+  #       "<ns:foo a=\"1\" a:b='2'>bar</ns:foo>",
+  #       "<ns:biz a='bar'/>",
+  #       "<foo><bar>baz</bar>\n</foo>",
+  #       "<foo><bar>one</bar><bar>two</bar></foo>",
+  # #      "<>bar</>",
+  #       "<foo>bar</baz>",
+  #       "<foo>bar</foo>oops",
+  #       "<foo>bar",
+  #       "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">",
+  #       "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:tns=\"http://www.witsml.org/wsdl/120\" xmlns:types=\"http://www.witsml.org/wsdl/120/encodedTypes\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soap:Body soap:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><q1:WMLS_GetVersion xmlns:q1=\"http://www.witsml.org/message/120\"/></soap:Body></soap:Envelope>",
+  #       "hi, this is a <bold>bold</bold> statement"
+  # ]
 
+  # for input <- inputs do
+  #     IO.puts(input)
+  #     stream = XMLStream.Parser.parse(input)
+  #     Enum.each(stream, fn x -> IO.inspect(x) end)
+  #     |> IO.inspect()
+  #     IO.puts("")
+  # end
 end

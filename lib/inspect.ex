7d660aa432f657a2aa6c1f79bbf53032f33179b6
Doug ClymerOlson <doug@yarina.org>
2024-03-29 17:27:21 +0000

    fixed so it will emit elements downstream

--- lib/inspect.ex
+++ lib/inspect.ex
@@ -9,6 +9,17 @@ defmodule XMLStreamTools.Inspector do
       |> XMLStreamTools.Transformer.transform(XMLStreamTools.Inspector.inspect_fn(label: "test_stream"))
       |> Enum.map(fn x -> x end)
 
+  This will show the stream as it is processed (see below), `path` is a seperate list inserted by the Transformer,
+  it shows the path from the root element.
+
+  ```
+  test_stream: {:open_tag, [tag: "foo", attr: [{"a", "1"}], loc: {{1, 0}, 1}]}, path: ["foo"]
+  test_stream: {:text, ["first element", {:loc, {{1, 0}, 24}}]}, path: ["foo"]
+  test_stream: {:open_tag, [tag: "bar", loc: {{1, 0}, 25}]}, path: ["bar", "foo"]
+  test_stream: {:text, ["nested element", {:loc, {{1, 0}, 43}}]}, path: ["bar", "foo"]
+  test_stream: {:close_tag, [tag: "bar", loc: {{1, 0}, 45}]}, path: ["bar", "foo"]
+  test_stream: {:close_tag, [tag: "foo", loc: {{1, 0}, 51}]}, path: ["foo"]
+  ```  
   """
 
   def inspect_fn(opts \\ []) do
@@ -16,8 +27,9 @@ defmodule XMLStreamTools.Inspector do
     fn element, path, acc -> inspect(element, path, acc, options) end
   end
 
-  def inspect({type, meta}, path, acc, %{label: label}) do
+  def inspect({type, meta} = element, path, acc, %{label: label}) do
     label = if label != nil, do: "#{label}: ", else: ""
     IO.puts("#{label}{#{inspect(type)}, #{inspect(meta)}}, path: #{inspect(path)}")
+    {element, []}  #always emit the element
   end
 end
